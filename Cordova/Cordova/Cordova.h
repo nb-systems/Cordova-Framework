16dc845f7a0ca4a7c6a407213ce781ffa2e1aa35
Nick Brighten <49920470+Nick-Brighten@users.noreply.github.com>
2019-05-06 19:59:22 +0200

    Add files via upload
    Added Xcode Project

---
+++ Cordova/Cordova/Cordova.h
@@ -0,0 +1,9 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double CordovaVersionNumber;
+FOUNDATION_EXPORT const unsigned char CordovaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cordova/PublicHeader.h>
+
+
