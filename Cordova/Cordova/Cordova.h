1b31fd924523733e76d5b91cf4dbfc85dedeb79b
Nick Brighten <sanbreeze@sanbreeze-macbook-pro.local>
2019-05-06 21:59:50 +0200

    Add Public Classes to Cordova.h

--- Cordova/Cordova/Cordova.h
+++ Cordova/Cordova/Cordova.h
@@ -26,6 +26,25 @@
 FOUNDATION_EXPORT double CordovaVersionNumber;
 FOUNDATION_EXPORT const unsigned char CordovaVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Cordova/PublicHeader.h>
-
-
+#import <Cordova/CDV.h>
+#import <Cordova/CDVAppDelegate.h>
+#import <Cordova/CDVAvailability.h>
+#import <Cordova/CDVAvailabilityDeprecated.h>
+#import <Cordova/CDVCommandDelegate.h>
+#import <Cordova/CDVCommandDelegateImpl.h>
+#import <Cordova/CDVCommandQueue.h>
+#import <Cordova/CDVConfigParser.h>
+#import <Cordova/CDVInvokedUrlCommand.h>
+#import <Cordova/CDVPlugin.h>
+#import <Cordova/CDVPlugin+Resources.h>
+#import <Cordova/CDVPluginResult.h>
+#import <Cordova/CDVScreenOrientationDelegate.h>
+#import <Cordova/CDVTimer.h>
+#import <Cordova/CDVUIWebViewDelegate.h>
+#import <Cordova/CDVURLProtocol.h>
+#import <Cordova/CDVUserAgentUtil.h>
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVWebViewEngineProtocol.h>
+#import <Cordova/CDVWhitelist.h>
+#import <Cordova/NSDictionary+CordovaPreferences.h>
+#import <Cordova/NSMutableArray+QueueAdditions.h>
